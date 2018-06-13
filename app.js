d0d95b27a40ecfe9046a13426ab3d66c5bb378e5
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-13 05:36:27 -0500

    declare Create Box function

--- app.js
+++ app.js
@@ -3,3 +3,9 @@
 var addButton = document.getElementById('add');
 var removeButton = document.getElementById('remove');
 var display = document.querySelector('.display');
+
+function createBlock() {
+  let newBox = document.createElement('div');
+  newBox.classList.add('box');
+  display.append(newBox);
+}
